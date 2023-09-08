7ad162a8effc0e48f09cc3333cb5b5b0c892ae38
Bill Sideris <bill88t@feline.gr>
2023-09-08 23:42:18 +0300

    Fix le makenposten

--- resources/make.py
+++ resources/make.py
@@ -1,11 +1,11 @@
 from sys import argv
 from sys import path as spath
 
-spath.append("./resources/CircuitMPY/")
-import circuitmpy
+spath.append("resources/circuitmpy")
+from circuitmpy import compile_mpy
 
 try:
-    circuitmpy.compile_mpy("src/ftp.py", "ftp_server.mpy", optim=3)
+    compile_mpy("src/ftp.py", "ftp_server.mpy", optim=3)
 except OSError:
     print("Compilation error, exiting")
     exit(1)
